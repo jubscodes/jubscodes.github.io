1e354cc65b7ce9840438900058306b7b1052c9f6
hoffms <jhoffmannburatto@gmail.com>
2026-05-01 13:12:43 -0300

    feat(work-list): list wrapper for work rows

---
+++ components/work-list.tsx
@@ -0,0 +1,7 @@
+export function WorkList({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="mx-auto max-w-5xl border-t border-border px-4">
+      {children}
+    </div>
+  );
+}
